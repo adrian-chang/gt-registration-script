c41e656fda82a42bdde4e1ed1d31cd184aa3398f
Adrian Chang <amchang87@gmail.com>
2016-01-10 19:19:17 -0800

    fix spacing issues

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #gt-registration-script
 
-This is a simple script which can be run to register for classes, view your schedule, or to view **CS / CSE** classes for the current semester, in an automated fashion at **Georgia Tech**.
+This is a simple script which can be run to register for classes, view your schedule, or to view **CS / CSE** classes in the **OMSCS** program at **Georgia Tech** for the current semester, in an automated fashion.
 
 For registration, it will not resolve waitlist issues or handle errors with classes being full.
 
@@ -9,6 +9,8 @@ When viewing your schedule, it will only display your schedule for the current s
 ** Use this script at your own risk. If Georgia Tech bans you for abusing their computing resources while using this script, it is your own responsibility to resolve your own ban. **
 
 ** It is not guaranteed to work if Georgia Tech changes [Oscar](http://oscar.gatech.edu). (Working as of 1/10/16) **
+
+** No password or username information is stored outside of your local machine. **
 
 ## Requirements
 1. `node 4.x` or higher
@@ -22,23 +24,27 @@ When viewing your schedule, it will only display your schedule for the current s
 2. `npm install`
 3. `npm run install` (will install selenium with chrome driver)
 
+
 ## Examples
 ```
-npm run start --
+npm run server (start the local default selenium example)
+
+node index.js -u 1234 -p 1234 -s 10 lookup
 ```
 
 ## Help
 ```
-node ./index.js "-h"
-
+node index.js
 Commands:
   register  register for classes
   lookup    lookup classes
+  schedule  look at your schedule
 
 Options:
   -u, --username  GT Username                                         [required]
   -p, --password  GT Password                                         [required]
-  -h, --help      Show help                                            [boolean]
+  -s, --seconds   Seconds to close browser after reaching the end of a command,
+                  0 = do not close automatically                   [default: 60]
 
 Examples:
   node index.js -u username -p password
--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const webdriverio = require('webdriverio'),
       yargs = require('yargs');
 
 // setup the actual driver options here
-const options = { 
-  desiredCapabilities: { 
-    browserName: 'chrome' 
-  } 
+const options = {
+  desiredCapabilities: {
+    browserName: 'chrome'
+  }
 };
-  
+
 // start the session
 let client = webdriverio.remote(options);
 
@@ -25,19 +25,23 @@ let argv = yargs.demand('u')
   .demand('u')
   .nargs('u', 1)
   // password and username
-  .describe('u', 'GT Username')
+  .describe('u', 'GT Student ID')
   .alias('p', 'password')
   .demand('p')
   .nargs('p', 1)
-  .describe('p', 'GT Password')
+  .describe('p', 'GT OSCAR Pin')
+  .describe('s', 'Seconds to close browser after reaching the end of a command, 0 = do not close automatically')
+  .default('s', 60)
+  .alias('s', 'seconds')
   .example('node $0 -u username -p password')
   .command('register', 'register for classes')
   .command('lookup', 'lookup classes')
-  .help('help')
+  .command('schedule', 'look at your schedule')
   .argv;
 
 // get the command
-const command = argv._[0];
+const command = argv._[0],
+      timeout = argv.s * 1000;
 
 // no command == help
 if (!command) {
@@ -50,32 +54,109 @@ client = client.init()
   .click('#securelogin')
   .addValue('#UserId', argv.u)
   .addValue('input[type="password"]', argv.p)
-  .click('input[type="submit"][value="Login"]');
+  .click('input[type="submit"][value="Login"]')
+  // inside now
+  .click('a[href="/pls/bprod/twbkwbis.P_GenMenu?name=bmenu.P_StuMainMnu"]')
+  .click('a[href="/pls/bprod/twbkwbis.P_GenMenu?name=bmenu.P_RegMnu"]');
 
 // let's figure out what else is needed for the command
 switch (command) {
   // let's register for a class
   case 'register':
+    // what do we need for register
     argv = yargs
       .reset()
+      // at least a class
       .array('c')
       .alias('c', 'classes')
+      .describe('c', 'Space delimited list of CRNs to use')
       .demand('c')
       .nargs('c', 1)
-      .usage('register')
+      .usage('$0 register')
       .example('$0 register -c 1234 4567')
+      .alias('s', 'submit')
+      .describe('s', 'Do we want to submit adding classes?')
+      .default('s', false)
       .help('h')
       .alias('h', 'help')
       .argv;
+
+    // go through adding
+    client = client
+      .click('a[href="/pls/bprod/bwskfreg.P_AltPin"]')
+      .submitForm('form[action="/pls/bprod/bwskfreg.P_AltPin"]');
+
+    // add each class here
+    const classes = argv.c,
+          max = Math.min(10, classes.length);
+    let c = 0;
+
+    for (c; c < max; c++) {
+      client = client.setValue('#crn_id' + (c + 1), classes[c]);
+    }
+
+    // submission
+    if (argv.s) {
+      console.dir('Submitting class changes');
+      client =
+        client.submitForm('form[action="/pls/bprod/bwckcoms.P_Regs"]');
+    } else {
+      console.dir('Submitting class changes');
+    }
     break;
   // I generally want to lookup all classes for this semester
   case 'lookup':
+    // what do we need for lookup
     argv = yargs
     .reset()
-    .usage('$0 lookup');
+    .usage('$0 lookup')
+    .help('h')
+    .alias('h', 'help')
+    .argv;
 
+    // go to specific page then click the most recent semester
+    client = client
+      .click('a[href="/pls/bprod/bwskfcls.p_sel_crse_search"]')
+      // seems to be an empty option
+      .selectByIndex('#term_input_id', 1)
+      .submitForm('form[action="/pls/bprod/bwckgens.p_proc_term_date"]')
+      // do advanced search
+      .click('input[value="Advanced Search"]')
+      // subjects and campus
+      .selectByValue('#subj_id', 'CS')
+      .selectByValue('#subj_id', 'CSE')
+      // unselect all
+      .selectByValue('#camp_id', '%')
+      .selectByValue('#camp_id', 'O')
+      .click('#advCourseBtnDiv input[type="Submit"]');
+
+    console.log('Showing current OMSCS Semester classes');
     break;
+  // view my schedule
   case 'schedule':
+    argv = yargs
+    .reset()
+    .usage('$0 lookup')
+    .help('h')
+    .alias('h', 'help')
+    .argv;
+
+    // simple
+    client = client
+      .click('a[href="/pls/bprod/bwskfshd.P_CrseSchdDetl"]');
+
+    client = client
+      .element('form[action="/pls/bprod/bwskfshd.P_CrseSchdDetl"] input[type="submit"]');
+
+    // ugly, but we need the last promise
+    client
+      .then((elem) => {
+        if (elem) {
+          return client.elementIdClick(elem.value.ELEMENT);
+        }
+      });
+
+    console.log('Showing my schedule');
     break;
   // unknown, show help
   default:
@@ -83,22 +164,13 @@ switch (command) {
     break;
 }
 
-// go through the shared menus
-client = client
-  .click('a[href="/pls/bprod/twbkwbis.P_GenMenu?name=bmenu.P_StuMainMnu"]')
-  .click('a[href="/pls/bprod/twbkwbis.P_GenMenu?name=bmenu.P_RegMnu"]');
-/*
-  /pls/bprod/bwskfcls.p_sel_crse_search
-
-client
-  .click('a[href="/pls/bprod/bwskfreg.P_AltPin"]');
-    /*.frame(client.element('frame[name="content"]'))
-    .setValue('#crn_id1', argv.c[0])
-    .setValue('#crn_id2', argv.c[1])
-    .click('input[type="submit"][value="Submit Changes"])')
-    .getTitle().then(function(title) {
-        console.log('Title is: ' + title);
-        // outputs: "Title is: WebdriverIO (Software) at DuckDuckGo"
-    });
-*/
-client.end();
+// if the timeout is 0, don't do anything
+if (timeout > 0) {
+  client.then(() => {
+    // close the app
+    setTimeout(() => {
+      console.log('Closing the application')
+       client.end();
+    }, timeout);
+  });
+}
